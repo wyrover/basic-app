2ebda5cc66273658e7b8816fc656f7119557e7f4
Nicolás Parada <parada.nicolas@outlook.com>
2015-11-15 07:37:32 -0300

    Reserved word

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict'
 const Koa = require('koa')
-const static = require('koa-static')
+const serve = require('koa-static')
 const router = require('./server/routes')
 const view = require('./server/utils/view')
 
@@ -9,7 +9,7 @@ const app = new Koa()
 app.use(router.routes())
 app.use(router.allowedMethods())
 
-app.use(static(__dirname + '/public'))
+app.use(serve(__dirname + '/public'))
 app.use(function *() {
 	this.body = yield view(__dirname + '/public/index.html')
 })
